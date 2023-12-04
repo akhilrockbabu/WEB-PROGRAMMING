e716cdaeac4e98d10fe0992c6b533b4e497387de
akhilrockbabu <akhilrockbabu@gmail.com>
2023-12-04 14:58:54 +0530

    hi

---
+++ productdetails.php
@@ -0,0 +1,56 @@
+<html>
+<body>
+<center>
+<?php
+//creating connection
+$con=new mysqli("localhost","root","","MCA");
+$q="select * from product";
+$r=$con->query($q);
+if($r->num_rows>0)
+{
+	?>
+	<table border="5" cellpadding="5" cellspacing="5">
+	<tr>
+	<td align="center" colspan="6"> PRODUCT DETAILS </td>
+	</tr>
+	<tr>
+	<td>ID</td>
+	<td>NAME</td>
+	<td>QUANTITY</td>
+	<td>UNIT PRICE</td>
+	<td>MANUFACTORING DATE</td>
+	<td>EXPIRY DATE</td>
+	</tr>
+	<?php
+	while($row=$r->fetch_assoc())
+	{
+		$id=$row['id'];
+		$name=$row['name'];
+		$qty=$row['qty'];
+		$unitp=$row['unitp'];
+		$manu=$row['manu'];
+		$exp=$row['exp'];
+		?>
+		<tr>
+		<td><?php echo $id;?></td>
+		<td><?php echo $name;?></td>
+		<td><?php echo $qty;?></td>
+		<td><?php echo $unitp;?></td>
+		<td><?php echo $manu;?></td>
+		<td><?php echo $exp;?></td>
+		</tr>
+		<?php
+	}
+}
+else
+	echo "please enter some details";
+$con->close();
+?>
+</table>
+</center>
+</body>
+</html>
+
+	
+	
+
